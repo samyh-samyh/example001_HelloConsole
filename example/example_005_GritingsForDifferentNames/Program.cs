8a058440b0f97c13ad236e0802b1d80247c470c0
Sergey Sidorov <89048598831s@gmail.com>
2022-12-03 17:13:19 +0300

    ex004 for usernames

---
+++ example/example_005_GritingsForDifferentNames/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter the username: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Hey, Masha! You`re look amazing!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
