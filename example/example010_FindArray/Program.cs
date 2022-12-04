28d998cf18248f5ff2c7ff90cf731f0ef639a71e
Sergey Sidorov <89048598831s@gmail.com>
2022-12-04 15:42:51 +0300

    end

---
+++ example/example010_FindArray/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+int [] array = {11, 18, 53, 46, 53, 62, 76, 18};
+
+int n= array.Length;
+int find=18;
+int index= 0;
+
+while (index<n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; //Чтобы при нахождении индекса
+               //с искомым числом программа не продолжала искать дальше
+    }
+    index++; // index=index+1
+}
---
+++ example/example011/Program.cs
@@ -0,0 +1,53 @@
+﻿void FillArray(int[] collection) // метод для заполнения массива рандомными числами
+{
+    int size = collection.Length;
+    int index=0;
+    while (index<size)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+
+    }
+}
+
+void PrintArray(int[] col) // Метод для печати массива
+{
+    int count = col.Length;
+    int position =0;
+    while (position<count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int Index00f(int [] collection, int find)
+{
+    int count = collection.Length;
+    int index=0;
+    int position=-1; //для определения того что элемент не найден
+
+    while (index<count)
+    {
+        if (collection[index]==find)
+        {
+            position=index;
+            break; //для завершения поиска position
+        }
+       index++; 
+    }
+    return position;
+}
+
+
+
+
+int [] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int pos=Index00f(array, 44);
+Console.WriteLine(pos);
+
+//метод Void подразумевает что он ничего не возвращает
