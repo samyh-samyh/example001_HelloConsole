5fa52574bf76f47c31b431594fda5eb9c298988c
Sergey Sidorov <89048598831s@gmail.com>
2022-12-04 14:54:39 +0300

    done

---
+++ example/example009_Arrays/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2>result) result=arg2;
+    if (arg3>result) result=arg3;
+    return result;
+}
+            //   0   1  2  3  4  5 6  7  8
+int [] array = {143,12,34,76,32,98,786,13,65};
+//array[0]=15; -обращение к тому что лежит в индексе массива и присвоение
+// Console.WriteLine(array[3]);  -вывод значения индекса
+
+
+int max= Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8]));
+Console.WriteLine(max);
